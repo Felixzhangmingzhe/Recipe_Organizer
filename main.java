27329c747607c2514eed4c0ebfc495fbe81c1990
Mingzhe Zhang <Felixzhangmingzhe@gmail.com>
2023-09-28 22:42:29 -0400

    Fix a git push error.

--- main.java
+++ main.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String API_URL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch";
+    private static final String API_KEY = "c3ab81c328msh9264d659cb4224cp11e1dbjsna6967482609d";
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        try {
+            sendGET();
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Error during API call", e);
+        }
+    }
+
+    private static void sendGET() throws Exception {
+        URL url = new URL(API_URL);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com");
+        connection.setRequestProperty("User-Agent", USER_AGENT);
+        connection.setRequestProperty("X-RapidAPI-Key", API_KEY);
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request failed");
+        }
+        // Done
+    }
 }
