a002be1d7399a64316d411acb248446a122d80fb
Mingzhe Zhang <Felixzhangmingzhe@gmail.com>
2023-11-25 15:55:26 -0500

    Add attribute to test whether it is in favorites.

--- src/entity/Recipe.java
+++ src/entity/Recipe.java
@@ -6,13 +6,15 @@ public class Recipe {
     private int id;
     private String title;
     private String content;
+    private boolean isFavorite;
 
     private LocalDateTime date;
-    public Recipe(int id, String title, String content, LocalDateTime date) {
+    public Recipe(int id, String title, String content, LocalDateTime date, boolean isFavorite) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.date = date;
+        this.isFavorite = isFavorite;
     }
     public int getId() {
         return this.id;
