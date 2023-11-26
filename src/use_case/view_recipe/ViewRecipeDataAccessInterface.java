7dd253fc606b3c32182f520b583521e58db4d0ea
KeepZoneqp <jamesqp.zheng@mail.utoronto.ca>
2023-11-26 16:41:55 -0500

    update FileRecipeDataAccessObject and ViewRecipeDataAccessInterface

--- src/use_case/view_recipe/ViewRecipeDataAccessInterface.java
+++ src/use_case/view_recipe/ViewRecipeDataAccessInterface.java
@@ -5,17 +5,5 @@ import entity.Recipe;
 import java.util.Map;
 
 public interface ViewRecipeDataAccessInterface {
-    public class InMemoryRecipeDataAccess implements ViewRecipeDataAccessInterface {
-        private Map<String, Recipe> recipes;
-
-        public InMemoryRecipeDataAccess(Map<String, Recipe> recipes) {
-            this.recipes = recipes;
-        }
-
-        @Override
-        public Recipe getRecipeById(String recipeId) {
-            return recipes.get(recipeId);
-        }
-    }
-
+    Recipe getRecipeById(String recipeId);
 }
