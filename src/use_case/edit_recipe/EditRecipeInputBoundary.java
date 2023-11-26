a68c2cee4381341f37498fc2b17a0e22d0ba3a1b
Mingzhe Zhang <Felixzhangmingzhe@gmail.com>
2023-11-25 20:42:16 -0500

    Complete the code for editing recipe use case and implement them.

--- src/use_case/edit_recipe/EditRecipeInputBoundary.java
+++ src/use_case/edit_recipe/EditRecipeInputBoundary.java
@@ -1,4 +1,5 @@
 package use_case.edit_recipe;
 
 public interface EditRecipeInputBoundary {
+    void execute(EditRecipeInputData inputData);
 }
--- src/use_case/edit_recipe/EditRecipeInteractor.java
+++ src/use_case/edit_recipe/EditRecipeInteractor.java
@@ -1,4 +1,16 @@
 package use_case.edit_recipe;
 
-public class EditRecipeInteractor {
+public class EditRecipeInteractor implements EditRecipeInputBoundary {
+    private final EditRecipeDataAccessInterface userDataAccess;
+    private final EditRecipeOutputBoundary presenter;
+
+    public EditRecipeInteractor(EditRecipeDataAccessInterface userDataAccess, EditRecipeOutputBoundary presenter) {
+        this.userDataAccess = userDataAccess;
+        this.presenter = presenter;
+    }
+
+    @Override
+    public void execute(EditRecipeInputData inputData) {
+
+    }
 }
