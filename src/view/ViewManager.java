dacc51d0ae8a17ce51245dfbb6708cad455714f4
KeepZoneqp <jamesqp.zheng@mail.utoronto.ca>
2023-11-26 20:00:15 -0500

    Update

--- src/view/ViewManager.java
+++ src/view/ViewManager.java
@@ -25,31 +25,4 @@ public class ViewManager implements PropertyChangeListener {
             String viewModelName = (String) evt.getNewValue();
             cardLayout.show(views, viewModelName);
         }
-import entity.Recipe;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-public class ViewManager {
-    private JFrame mainFrame;
-
-    public ViewManager(JFrame mainFrame) {
-        this.mainFrame = mainFrame;
-    }
-
-    public void changeView(JPanel newView) {
-        mainFrame.getContentPane().removeAll();
-        mainFrame.getContentPane().add(newView);
-        mainFrame.revalidate();
-        mainFrame.repaint();
-    }
-
-    public void showWarehouseView(List<Recipe> recipes) {
-        WarehouseView warehouseView = new WarehouseView();
-        warehouseView.setRecipes(recipes);
-        changeView(warehouseView);
-    }
 }
-
