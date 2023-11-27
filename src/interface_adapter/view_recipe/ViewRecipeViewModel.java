c086f80e8ccdd9a8dfe9a9426432d35b19127f1c
KeepZoneqp <jamesqp.zheng@mail.utoronto.ca>
2023-11-26 20:00:15 -0500

    Update

---
+++ src/interface_adapter/view_recipe/ViewRecipeViewModel.java
@@ -0,0 +1,28 @@
+package interface_adapter.view_recipe;
+
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+
+public class ViewRecipeViewModel extends ViewModel {
+
+    private ViewRecipeState state;
+
+    public ViewRecipeViewModel() {
+        super("view_recipe");
+    }
+
+    @Override
+    public void firePropertyChanged() {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+
+    }
+
+    public
+
+
+}
