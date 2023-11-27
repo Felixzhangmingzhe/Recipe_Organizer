57bd7f186606028dc9aa801521247cee4dbaadf2
KeepZoneqp <jamesqp.zheng@mail.utoronto.ca>
2023-11-27 01:46:41 -0500

    Update  View Recipe in interface_adapter  package

--- src/interface_adapter/view_recipe/ViewRecipeViewModel.java
+++ src/interface_adapter/view_recipe/ViewRecipeViewModel.java
@@ -1,28 +1,32 @@
 package interface_adapter.view_recipe;
 
+import entity.Recipe;
 import interface_adapter.ViewModel;
 
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 public class ViewRecipeViewModel extends ViewModel {
 
-    private ViewRecipeState state;
+    private Recipe recipe;
 
     public ViewRecipeViewModel() {
-        super("view_recipe");
+        super("viewRecipeViewModel");
     }
 
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+    public Recipe getRecipe() {
+        return recipe;
+    }
+    private PropertyChangeSupport support = new PropertyChangeSupport(this);
     @Override
     public void firePropertyChanged() {
-
+        support.firePropertyChange("recipe", null, recipe);
     }
-
     @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {
-
+        support.addPropertyChangeListener(listener);
     }
-
-    public
-
-
 }
