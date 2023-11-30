145e9b6eac6d45198d3983a3536cec2859107a5d
ariahu <aria.hu@mail.utoronto.ca>
2023-11-30 02:44:32 -0500

    Added several View Tests; Moved one file into its corresponding package

---
+++ src/test/view_test/FavoritesViewTest.java
@@ -0,0 +1,34 @@
+package view_test;
+
+import org.junit.Before;
+import org.junit.Test;
+import view.FavoritesView;
+
+import static org.junit.Assert.*;
+
+public class FavoritesViewTest {
+
+    private FavoritesView favoritesView;
+
+    @Before
+    public void setUp() {
+        // 初始化 FavoritesView 实例
+        favoritesView = new FavoritesView();
+    }
+
+    @Test
+    public void testTotalFavoritesPanelNotNull() {
+        // 检查 TotalFavoritesPanel 是否被正确初始化
+        assertNotNull("TotalFavoritesPanel 不应为 null", favoritesView.getTotalfavoritesPanel());
+    }
+
+    @Test
+    public void testFavoritesPanelNotNull() {
+        // 检查 FavoritesPanel 是否被正确初始化
+        assertNotNull("FavoritesPanel 不应为 null", favoritesView.getFavoritesPanel());
+    }
+
+    // 测试 showTotalFavorites 和 showSingleFolder 方法
+    // 这里的测试可能需要根据实际的 GUI 行为进行调整
+    // 由于这些方法涉及到显示 JFrame，可能需要特定的策略来测试 GUI 行为
+}
---
+++ src/test/view_test/WarehouseViewTest.java
@@ -0,0 +1,37 @@
+package view_test;
+
+import interface_adapter.view_recipe.ViewRecipeController;
+import org.junit.Before;
+import org.junit.Test;
+import view.WarehouseView;
+
+import javax.swing.*;
+import static org.junit.Assert.*;
+
+public class WarehouseViewTest {
+
+    private WarehouseView warehouseView;
+
+    @Before
+    public void setUp() {
+        // 创建 ViewRecipeController 的存根
+        // 存根方法的实现，可以根据需要添加逻辑
+        ViewRecipeController mockViewRecipeController = new ViewRecipeController() {
+            @Override
+            public void execute(int recipeId) {
+                // 存根方法的实现，可以根据需要添加逻辑
+            }
+        };
+
+        // 初始化 WarehouseView 实例
+        warehouseView = new WarehouseView(mockViewRecipeController);
+    }
+
+    @Test
+    public void testWarehousePanelNotNull() {
+        // 测试 WarehousePanel 是否正确初始化
+        assertNotNull("WarehousePanel should not be null", warehouseView.getWarehousePanel());
+    }
+
+
+}
