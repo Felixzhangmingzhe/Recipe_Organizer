7af8c263be389c11c0ada141bf7f65e300bc0b49
KeepZoneqp <jamesqp.zheng@mail.utoronto.ca>
2023-11-30 01:03:57 -0500

    create FavoritesViewUseCaseFactory.java

--- src/app/FavoritesViewUseCaseFactory.java
+++ src/app/FavoritesViewUseCaseFactory.java
@@ -17,22 +17,32 @@ import use_case.view_recipe.ViewRecipeOutputBoundary;
 import view.FavoritesView;
 import view.WarehouseView;
 
+
+
 public class FavoritesViewUseCaseFactory {
-    public static FavoritesView create(ViewRecipeViewModel viewRecipeViewModel, ViewManagerModel viewManagerModel, FileRecipeDataAccessObject dao, BackViewModel backViewModel) {
+    public static FavoritesView create(ViewRecipeViewModel viewRecipeViewModel,
+                                       ViewManagerModel viewManagerModel,
+                                       FileRecipeDataAccessObject dao, BackViewModel backViewModel) {
         ViewRecipeController viewRecipeController1 = createViewRecipeController(viewRecipeViewModel, viewManagerModel, dao);
         BackController backController = createBackController(backViewModel, viewManagerModel);
         return new FavoritesView(viewRecipeController1, viewRecipeViewModel, backController,viewManagerModel);
     }
 
-    private static ViewRecipeController createViewRecipeController(ViewRecipeViewModel viewRecipeViewModel, ViewManagerModel viewWarehouseViewModel, FileRecipeDataAccessObject dao) {
+
+    private static ViewRecipeController createViewRecipeController(ViewRecipeViewModel viewRecipeViewModel,
+                                                                   ViewManagerModel viewWarehouseViewModel,
+                                                                   FileRecipeDataAccessObject dao) {
         ViewRecipeOutputBoundary viewRecipeOutputBoundary = new ViewRecipePresenter(viewRecipeViewModel, viewWarehouseViewModel);
         ViewRecipeInputBoundary viewRecipeInputBoundary = new ViewRecipeInteractor(dao, (ViewRecipePresenter) viewRecipeOutputBoundary);
         return new ViewRecipeController(viewRecipeInputBoundary);
     }
+
+
     private static BackController createBackController(BackViewModel backViewModel, ViewManagerModel viewManagerModel) {
         BackOutputBoundary backOutputBoundary = new BackPresenter(viewManagerModel, backViewModel);
         BackInputBoundary backInputBoundary = new BackInteractor(backOutputBoundary);
         return new BackController(backInputBoundary);
+
     }
 }
 
