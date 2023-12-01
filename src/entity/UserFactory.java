0223e482453fa067b90322f76b9a8f07a9551a58
ariahu <aria.hu@mail.utoronto.ca>
2023-11-30 21:58:05 -0500

    Did some modification in entities

--- src/entity/UserFactory.java
+++ src/entity/UserFactory.java
@@ -5,5 +5,8 @@ public class UserFactory {
     public User create(int id, String username, String password) {
         return new User(id, username, password);
     }
+
+//** Requires: password is valid.
+
 }
 // 我们不要你了
