4622b6217f68237fd4821c7e4ea740806f55df11
Mingzhe Zhang <Felixzhangmingzhe@gmail.com>
2023-12-01 16:27:56 -0500

    Update in AddtoFavorites Usecase

--- src/use_case/add_to_favorites/AddToFavoritesInputData.java
+++ src/use_case/add_to_favorites/AddToFavoritesInputData.java
@@ -4,27 +4,12 @@ import entity.Recipe;
 
 public class AddToFavoritesInputData {
     private final String title;
-    private final int id;
-    private final String title;
-
-    public AddToFavoritesInputData(String title, int id) {
-        this.id = id;
-        this.title = title;
-    }
 
     public AddToFavoritesInputData(String title) {
         this.title = title;
-    public String getTitle() {
-        return title;
     }
 
     public String getTitle() {
         return title;
-    int getId() {
-        return id;
     }
-
-//    public int getId() {
-//
-//    }
 }
